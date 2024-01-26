a6b1e7470baa898a0ae513cef2b84389112aff82
Arsen Chernov <arsenchernov02@gmail.com>
2024-01-26 08:11:18 +0600

    Added a function that saves data in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MovieForm from "./Components/MovieForm/Form";
 import MovieItem from "./Components/Movies/MovieItem";
 import IdGenerator from "./Components/IdGenerator/IdGenerator";
 import { Movie } from "./types";
 
 const App = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>(() => {
+    const savedData = localStorage.getItem('movies');
+
+    return savedData ? JSON.parse(savedData) : [];
+  });
+
+  useEffect (() => {
+    localStorage.setItem('movies', JSON.stringify(movies));
+  });
 
   const addMovie = (title: string) => {
     setMovies([...movies, {id: IdGenerator(), title }]);
